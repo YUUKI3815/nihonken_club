0d26c31ed8fe59a95800356b20de20e903c9177f
YUUKI3815 <s.yuuki3815@gmail.com>
2022-05-12 12:09:49 +0000

    add pets

--- app/controllers/public/pets_controller.rb
+++ app/controllers/public/pets_controller.rb
@@ -1,14 +1,25 @@
 class Public::PetsController < ApplicationController
 def index
-  
+  #ログインしているユーザーのペットのみを表示
+  @pets = current_user.pets
+  @pet = Pet.new
 end
 
 def create
-  
+  @pets = current_user.pets
+  @pet = Pet.new
+  #ペットとユーザーIDの紐付け
+  @pet.user_id = current_user.id
+  if @pet.save
+    redirect_to action: :index
+  else
+    @pets = current_user.pets
+    render :index
+  end
 end
 
 def edit
-  
+  @pet = Pet.find(params[:id])
 end
 
 def update
@@ -17,5 +28,11 @@ end
 
 def destroy
   
+end
+
+private
+
+def pet_params
+  params.require(:pet).permit(:name, :introduction, :age, :gender, :profile_image)
 end
 end
